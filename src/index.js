280d79e44f127b2437ce831a4ecfd4d0240fbcf8
Martina Hytychová <martina.hyt@gmail.com>
2020-11-01 11:40:25 +0100

    interakce v mobilním menu

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import './index.html';
 import './style.css';
 
-console.log('funguju!');
+const navbar = document.querySelector('#nav-btn');
+const navbarItems = document.querySelectorAll('.nav-item');
+const nav = document.querySelector('nav');
+
+navbar.addEventListener('click', () => {
+  nav.classList.toggle('nav-closed');
+});
+
+for (let i = 0; i < navbarItems.length; i++) {
+  const navigation = navbarItems[i];
+  navigation.addEventListener('click', () => {
+    nav.classList.toggle('nav-closed');
+  });
+}
