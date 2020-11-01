4502425b2653de97a7c6f52a57d22bc769e49d4c
Martina Hytychová <martina.hyt@gmail.com>
2020-11-01 15:21:54 +0100

    vložení komponenty Layer do komponenty Drink

--- src/Layer/index.js
+++ src/Layer/index.js
@@ -1,9 +1,11 @@
 import './style.css';
 
 // komponenta pro layer
-export const Layer = ({ color, label }) => {
+export const Layer = (props) => {
   const layerElm = document.createElement('div');
-  layerElm.setAttribute('class', 'layer');
-  layerElm.innerHTML = `<div class="layer__color" style="background-color: ${color}"></div><div class="layer__label">${label}</div>`;
+  for (let i = 0; i < props.length; i++) {
+    layerElm.setAttribute('class', 'layer');
+    layerElm.innerHTML += `<div class="layer__color" style="background-color: ${props[i].color}"></div><div class="layer__label">${props[i].label}</div>`;
+  }
   return layerElm;
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.html';
 import './style.css';
-import { Layer } from './Layer';
+import { Drink } from './Drink';
 
 //====================== MENU ===========================//
 
@@ -22,56 +22,25 @@ for (let i = 0; i < navbarItems.length; i++) {
   });
 }
 
-//====================== ORDER ===========================//
+//====================== COFEE PRODUCTS ===========================//
 
-// zachytit elementy pro objednávku
-const orderButton = document.querySelector('.order-btn');
-const drinkCup = document.querySelector('.drink__cup');
+// objekt s properties pro komponentu Drink
+const productObj = {
+  id: 'romano',
+  name: 'Romano',
+  ordered: false,
+  layers: [
+    {
+      color: '#fbdf5b',
+      label: 'citrón',
+    },
+    {
+      color: '#613916',
+      label: 'espresso',
+    },
+  ],
+};
 
-// globální proměnná s Boolean hodnotou
-let ordered = true;
-
-// na kliknutí vybrat kafe nebo zrušit objednávku
-orderButton.addEventListener('click', () => {
-  if (ordered) {
-    orderButton.innerHTML = 'Zrušit';
-    ordered = false;
-  } else {
-    orderButton.innerHTML = 'Objednat';
-    ordered = true;
-  }
-  drinkCup.classList.toggle('drink__cup--selected');
-});
-
-//====================== COFEE PRODUCTS/INGREDIENCES ===========================//
-
-// testovací vrstva pro komponentu Layer
-/*const layer = {
-  color: '#feeeca',
-  label: 'mléčná pěna',
-};*/
-
-// vrstvy pro komponentu Layer
-const layers = [
-  {
-    color: '#feeeca',
-    label: 'mléčná pěna',
-  },
-  {
-    color: '#fed7b0',
-    label: 'teplé mléko',
-  },
-  {
-    color: '#613916',
-    label: 'espresso',
-  },
-];
-
-// zachytit element, kam vložím HTML komponentu
-const product = document.querySelector('.drink__info');
-
-// přidat všechny 3 vrstvy
-for (let j = 0; j < layers.length; j++) {
-  product.appendChild(Layer(layers[j]));
-}
-
+// zachytit element, kam vložím komponentu Drink
+const product = document.querySelector('.drinks-list');
+product.appendChild(Drink(productObj));
